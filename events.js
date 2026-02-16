4e013efbf2df201dbd539094e62b63b3fd5dc542
WAHVZ <waliashraf42@gmail.com>
2026-02-16 20:06:56 +0500

    initial commit: add basic age modifier page

---
+++ events.js
@@ -0,0 +1,15 @@
+var numOne = document.getElementById("Num_one");
+var numTwo = document.getElementById("Num_two");
+var Sum = document.getElementById("add_sum");
+
+numOne.addEventListener("input", function(){
+    console.log("Yes, type in the box a number.")
+});
+
+numTwo.addEventListener("click", function(){
+    console.log("Type in 2.")
+})
+
+addEventListener("click", function(){
+    console.log("Why are you clicking randomly?")
+})
